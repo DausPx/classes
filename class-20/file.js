b6a3db3a814f4656ae78f3d13757b22c58a5eef2
Suad Suljovic <suuad@live.com>
2022-04-07 17:55:38 +0200

    class 22

---
+++ class-20/file.js
@@ -0,0 +1,27 @@
+// var a = 20;
+// var b = 30;
+// var c = 100;
+
+// if (a > b) {
+//   if (a > c) {
+//     console.log(`veci broj je ${a}`);
+//   } else {
+//     console.log(`veci broj je ${c}`);
+//   }
+// } else {
+//   if (b > c) {
+//     console.log(`veci broj je ${b}`);
+//   } else {
+//     console.log(`veci broj je ${c}`);
+//   }
+// }
+
+var array = [1, 12, 13, 123];
+var arrayb = [1, 2];
+
+// array.push("str");
+// array.pop();
+// array.unshift("sdf");
+// array.shift();
+
+console.log(c);
