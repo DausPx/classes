58e9cb9ec4e3474f3c8e749bf30a7849c5b709f1
Suad Suljovic <suad.suljovic92@gmail.com>
2022-05-14 18:30:09 +0200

    extra fetching

--- class-34/index.js
+++ class-34/index.js
@@ -1,13 +1,10 @@
 const fetch = require("node-fetch");
-const fn = async (args) => {
-  const author = args[0];
-
+const fn = async (author) => {
   const response = await fetch(
     `https://api.quotable.io/random${author ? `?author=${author}` : ""}`
   );
   const result = await response.json();
 
-  console.log(result);
   console.log(result.content + "; author: " + result.author);
 };
 
