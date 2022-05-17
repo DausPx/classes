271f29306d7258d5caf929a7aafc7a516e65a1b1
Suad Suljovic <suad.suljovic92@gmail.com>
2022-05-17 18:56:28 +0200

    fetching 4

--- class-34/index.js
+++ class-34/index.js
@@ -1,13 +1,20 @@
 const fetch = require("node-fetch");
-const fn = async (author) => {
+const randomQuoteFromAuthor = async (author) => {
   try {
+    const checkedAuthor = await authorCheck(author);
+
+    if (!checkedAuthor) {
+      return "That author doesn't exist";
+    }
+
     const response = await fetch(
-      `https://api.quotable.io/random${author ? `?author=${author}` : ""}`
+      `https://api.quotable.io/random${
+        checkedAuthor ? `?author=${checkedAuthor}` : ""
+      }`
     );
     const result = await response.json();
-    console.log(result);
 
-    console.log(result.content + "; author: " + result.author);
+    return result.content + ", Author: " + result.author;
   } catch (error) {
     console.log(error);
   }
@@ -19,15 +26,36 @@ const authorCheck = async (author) => {
       `https://api.quotable.io/search/authors?query=${author}`
     );
     const result = await response.json();
-    console.log(result);
-    if (result.count < 1) {
+
+    if (!result.count) {
       return false;
     }
-
     return result.results[0].name;
   } catch (error) {
     console.log(error);
   }
 };
 
-authorCheck("Adam").then((value) => console.log(value));
+const getQuotes = async (page, sortBy, order) => {
+  try {
+    const paramsExist = page || sortBy || order;
+    const response = await fetch(
+      `https://api.quotable.io/quotes${paramsExist ? "?" : ""}${
+        page ? `page=${page}` : ""
+      }${page ? "&" : ""}${sortBy ? `sortBy=${sortBy}` : ""}${
+        page || sortBy ? "&" : ""
+      }${order ? `order=${order}` : ""}`
+    );
+    const result = await response.json();
+    return result.results;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getQuotes(95, "content", "desc").then((result) => {
+  for (let i = 0; i < result.length; i++) {
+    const element = result[i];
+    console.log(element.content + " by: " + element.author);
+  }
+});
