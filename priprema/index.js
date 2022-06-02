93e79356b016d8e42e6da41bafe51c82b8aeb95d
Suad Suljovic <suad.suljovic92@gmail.com>
2022-06-02 18:45:38 +0200

    priprema

---
+++ priprema/index.js
@@ -0,0 +1,18 @@
+// let, const i var
+// truthy vs falsy and conditionals
+// functions/ callbacks
+// scope and this
+// hoisting
+// promises
+// async i await
+// closure
+
+function name1(a) {
+  return {
+    inner: () => {
+      console.log("suad");
+    },
+  };
+}
+
+name1(6).inner();
