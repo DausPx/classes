d6e7b91e280d2c1cb4e3a593a0f219c2f549de8e
Suad Suljovic <suad.suljovic92@gmail.com>
2022-06-17 14:58:18 +0200

    test correct answers

--- class-39/script.js
+++ class-39/script.js
@@ -1,68 +1,53 @@
-const getQuotes = async (page = 1) => {
+const getQuotes = async () => {
   try {
-    const response = await fetch(
-      `https://api.quotable.io/quotes?${page ? `page=${page}` : ""}`
-    );
+    const response = await fetch(`https://api.imgflip.com/get_memes`);
 
     const result = await response.json();
 
-    return result.results;
+    return result.data.memes;
   } catch (error) {
     console.log(error);
   }
 };
 
-let page = 1;
 const contentDiv = document.getElementById("content");
-const pageInput = document.getElementById("page");
+// const pageInput = document.getElementById("page");
 
-// const createCard = (author, quote) => {
-//   return `<div
-//  style="
-//    width: 400px;
-//    min-height: 100px;
-//    border: 1px solid black;
-//    margin: 10px;
-//    padding: 20px;
-//  "
-// >
-//  <p>
-//    ${quote}
-//  </p>
-//  <p>By: ${author}</p>
-// </div>`;
-// };
-
-// getQuotes().then((result) => {
-//   result.forEach((quote) => {
-//     console.log(contentDiv.innerHTML);
-//     contentDiv.innerHTML =
-//       contentDiv.innerHTML + createCard(quote.author, quote.content);
-//   });
-//   //   contentDiv.appendChild();
-// });
-
-const createCardNode = (author, quote) => {
-  const div = document.createElement("div");
-  div.style = ` width: 400px;
-       min-height: 100px;
-       border: 1px solid black;
-       margin: 10px;
-       padding: 20px;
-     `;
-
-  const p = document.createElement("p");
-  p.textContent = quote;
-
-  div.appendChild(p);
-
-  const p2 = document.createElement("p");
-  p2.textContent = `By: ${author}`;
-
-  div.appendChild(p2);
-
-  return div;
+const createCard = (data) => {
+  return `<div style="border: 1px solid black; margin: 10px; width: 500px">
+  <p style="text-align: center">${data.name}</p>
+  <img src="${data.url}" alt="" width="100%" />
+</div>`;
 };
+
+getQuotes().then((result) => {
+  result.forEach((data) => {
+    console.log(contentDiv.innerHTML);
+    contentDiv.innerHTML = contentDiv.innerHTML + createCard(data);
+  });
+});
+
+// const createCardNode = (author, quote) => {
+//   const div = document.createElement("div");
+//   div.style = ` width: 400px;
+//        min-height: 100px;
+//        border: 1px solid black;
+//        margin: 10px;
+//        padding: 20px;
+//      `;
+
+//   const p = document.createElement("p");
+//   p.textContent = quote;
+
+//   div.appendChild(p);
+
+//   const p2 = document.createElement("p");
+//   p2.textContent = `By: ${author}`;
+
+//   div.appendChild(p2);
+
+//   return div;
+// };
 
 // getQuotes().then((result) => {
 //   result.forEach((quote) => {
@@ -71,16 +56,16 @@ const createCardNode = (author, quote) => {
 //   });
 // });
 
-const clickable = () => {
-  getQuotes(pageInput.value).then((result) => {
-    contentDiv.innerHTML = "";
-    result.forEach((quote) => {
-      const newCard = createCardNode(quote.author, quote.content);
-      contentDiv.appendChild(newCard);
-    });
-  });
-};
+// const clickable = () => {
+//   getQuotes(pageInput.value).then((result) => {
+//     contentDiv.innerHTML = "";
+//     result.forEach((quote) => {
+//       const newCard = createCardNode(quote.author, quote.content);
+//       contentDiv.appendChild(newCard);
+//     });
+//   });
+// };
 
-const button = document.getElementById("click");
+// const button = document.getElementById("click");
 
-button.addEventListener("click", clickable);
+// button.addEventListener("click", clickable);
--- priprema/index.js
+++ priprema/index.js
@@ -7,12 +7,27 @@
 // async i await
 // closure
 
-function name1(a) {
-  return {
-    inner: () => {
-      console.log("suad");
-    },
-  };
-}
+// const array1 = [
+//   { id: "333222111", name: "Sulejman", age: "22" },
+//   { id: "321123432", name: "Stefan", age: "25" },
+//   { id: "222312543", name: "Semir", age: "32" },
+// ];
 
-name1(6).inner();
+// function getObjects(array, key, value) {
+//   const result = array.filter((obj) => {
+//     if (obj[key] === value) {
+//       return true;
+//     }
+//     return false;
+//   });
+
+//   // const result = []
+//   // array.forEach((obj) => {
+//   //   if (obj[key] === value) {
+//   //     result.push(obj);
+//   //   }
+//   // });
+//   return result;
+// }
+
+// console.log(getObjects(array1, "name", "Suad"));
